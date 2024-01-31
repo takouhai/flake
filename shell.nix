9555eb2f35fad428f4851719987fda19ea0bb3fe
taco <daniel@tacowolf.net>
2024-01-30 20:12:26 -0500

    fleek: apply

--- shell.nix
+++ shell.nix
@@ -10,19 +10,11 @@
     programs.bat.config = {
   theme = "TwoDark";
 };
-    # bash
-  programs.bash.profileExtra = ''
+    # zsh
+  programs.zsh.profileExtra = ''
     [ -r ~/.nix-profile/etc/profile.d/nix.sh ] && source  ~/.nix-profile/etc/profile.d/nix.sh
     export XCURSOR_PATH=$XCURSOR_PATH:/usr/share/icons:~/.local/share/icons:~/.icons:~/.nix-profile/share/icons
-
   '';
-  programs.bash.initExtra = ''
-    if [ -f /etc/bashrc ]; then
-        . /etc/bashrc
-    fi
-    source <(fleek completion bash)
-  '';
-  programs.bash.enableCompletion = true;
-  programs.bash.enableVteIntegration = true;
-  programs.bash.enable = true;
+  programs.zsh.enableCompletion = true;
+  programs.zsh.enable = true;
 }
