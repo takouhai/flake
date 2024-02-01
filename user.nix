0147d796422e1775a09a884247c0f088ef194083
taco <daniel@tacowolf.net>
2024-02-01 17:43:38 -0500

    fleek: apply

--- user.nix
+++ user.nix
@@ -1,5 +1,8 @@
 { pkgs, misc, ... }: {
   # FEEL FREE TO EDIT: This file is NOT managed by fleek. 
 
- 
+  programs.zsh.oh-my-zsh = {
+    enable = true;
+    plugins = [ "git" "thefuck" "volta" "rust" "rvm" "zoxide"];
+  }
 }
