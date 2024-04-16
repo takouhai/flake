f23115d5949462972fd5ec847e24abd948b8089c
taco <daniel@tacowolf.net>
2024-04-16 18:27:37 -0400

    update readme and sort some things

--- README.md
+++ README.md
@@ -1,34 +1,21 @@
-# taco on fleek
+# taco on flake
 
-nix home-manager configs created by [fleek](https://github.com/ublue-os/fleek).
+nix home-manager configs created by [fleek](https://github.com/ublue-os/fleek), which died. so now it's my own frosted flake config.
 
-## Reference
+## reference
 
 - [home-manager](https://nix-community.github.io/home-manager/)
 - [home-manager options](https://nix-community.github.io/home-manager/options.html)
 
-## Usage
+## usage
 
-Aliases were added to the config to make it easier to use. To use them, run the following commands:
+aliases were added by fleek to the config to make it easier to use. to use them, run the following command:
 
 ```bash
-# To change into the fleek generated home-manager directory
-$ fleeks
-# To apply the configuration
 $ apply-$(hostname)
 ```
-
-Your actual aliases are listed below:
-    apply-fishcrow = "nix run --impure home-manager/master -- -b bak switch --flake .#taco@fishcrow";
-
-    apply-myna = "nix run --impure home-manager/master -- -b bak switch --flake .#taco@myna";
-
-    fleeks = "cd ~/.local/share/fleek";
-
-    latest-fleek-version = "nix run https://getfleek.dev/latest.tar.gz -- version";
-
-    please = "sudo";
-
-    ssh-agent-man = "echo \"THEY'VE GIVEN YOU A NUMBER\" && eval `ssh-agent -s` ssh-add && echo \"AND TAKEN AWAY YOUR NAME\"";
-
-    update-fleek = "nix run https://getfleek.dev/latest.tar.gz -- update";
+for example:
+```
+$ apply-myna
+```
+on `myna`.
--- home.nix
+++ home.nix
@@ -1,34 +1,35 @@
 { config, pkgs, misc, ... }: {
-  # DO NOT EDIT: This file is managed by fleek. Manual changes will be overwritten.
   nixpkgs = {
-    # Configure your nixpkgs instance
     config = {
-      # Disable if you don't want unfree packages
-      
       allowUnfree = true;
-      # Workaround for https://github.com/nix-community/home-manager/issues/2942
       allowUnfreePredicate = (_: true);
-      
-      
     };
   };
-
   
   # packages are just installed (no configuration applied)
   # programs are installed and configuration applied to dotfiles
   home.packages = [
-    # user selected packages
     pkgs.asciiquarium
+    pkgs.btop
+    pkgs.cheat
     pkgs.dog
     pkgs.du-dust
+    pkgs.fzf
+    pkgs.git
+    pkgs.github-cli
+    pkgs.glab
     pkgs.glances
+    pkgs.glow
     pkgs.gping
     pkgs.helix
     pkgs.hexyl
+    pkgs.htop
     pkgs.httpie
     pkgs.hyperfine
     pkgs.iotop
     pkgs.jq
+    pkgs.jq
+    pkgs.just
     pkgs.lazygit
     pkgs.lsd
     pkgs.neofetch
@@ -40,33 +41,20 @@
     pkgs.pinentry-curses
     pkgs.pls
     pkgs.ripgrep
+    pkgs.ripgrep
     pkgs.shellcheck
     pkgs.sl
     pkgs.thefuck
     pkgs.tldr
     pkgs.tmatrix
     pkgs.volta
-    pkgs.xplr
-    # Fleek Bling
-    pkgs.git
-    pkgs.htop
-    pkgs.github-cli
-    pkgs.glab
-    pkgs.fzf
-    pkgs.ripgrep
     pkgs.vscode
-    pkgs.lazygit
-    pkgs.jq
+    pkgs.xplr
     pkgs.yq-go
-    pkgs.neovim
-    pkgs.neofetch
-    pkgs.btop
-    pkgs.cheat
-    pkgs.just
     (pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; })
   ];
   fonts.fontconfig.enable = true; 
   home.stateVersion =
-    "22.11"; # To figure this out (in-case it changes) you can comment out the line and see what version it expected.
+   # "23.11"; # To figure this out (in-case it changes) you can comment out the line and see what version it expected.
   programs.home-manager.enable = true;
 }
