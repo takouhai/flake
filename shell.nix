823a827df75ce236506560dbb191c8c2f4062162
taco <daniel@tacowolf.net>
2024-04-16 19:19:54 -0400

    change shell to be nvim

--- shell.nix
+++ shell.nix
@@ -1,19 +1,22 @@
 { pkgs, misc, ... }: {
-    programs.eza.enableAliases = true;
-    
-    programs.eza.extraOptions = [
-   "--group-directories-first"
-   "--header"
-];
-    
-    programs.bat.config = {
-  theme = "TwoDark";
-};
-    # zsh
+  programs.eza.enableAliases = true;
+  
+  programs.eza.extraOptions = [
+    "--group-directories-first"
+    "--header"
+  ];
+  
+  programs.bat.config = {
+    theme = "TwoDark";
+  };
+  # zsh
   programs.zsh.profileExtra = ''
-    [ -r ~/.nix-profile/etc/profile.d/nix.sh ] && source  ~/.nix-profile/etc/profile.d/nix.sh
-    export XCURSOR_PATH=$XCURSOR_PATH:/usr/share/icons:~/.local/share/icons:~/.icons:~/.nix-profile/share/icons
+  [ -r ~/.nix-profile/etc/profile.d/nix.sh ] && source  ~/.nix-profile/etc/profile.d/nix.sh
+  export XCURSOR_PATH=$XCURSOR_PATH:/usr/share/icons:~/.local/share/icons:~/.icons:~/.nix-profile/share/icons
   '';
   programs.zsh.enableCompletion = true;
   programs.zsh.enable = true;
+  programs.zsh.localVariables = {
+    EDITOR = "nvim";
+  };
 }
