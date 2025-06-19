0d2bbb3ea71cf742202cf8ba19ea8cd757b7ebe2
taco <gpg@tacowolf.net>
2025-06-19 11:23:23 -0400

    feat: add playdate sdk to one machine

--- crow/taco.nix
+++ crow/taco.nix
@@ -1,4 +1,10 @@
 {...}: {
   home.username = "taco";
   home.homeDirectory = "/home/taco";
+  home.sessionPath = [
+    "/home/taco/Utilities/PlaydateSDK-2.7.1/bin"
+  ];
+  programs.zsh.localVariables = {
+    PLAYDATE_SDK_PATH = "/home/taco/Utilities/PlaydateSDK-2.7.1";
+  };
 }
