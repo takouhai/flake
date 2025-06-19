819b23d164856c9769f9057cec88f14f82ac05ba
taco <gpg@tacowolf.net>
2025-06-19 19:37:27 -0400

    feat: add wezterm config file

---
+++ .wezterm.lua
@@ -0,0 +1,5 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+config.color_scheme = 'Andromeda'
+config.font = wezterm.font 'ComicShannsMono Nerd Font Mono'
+return config
