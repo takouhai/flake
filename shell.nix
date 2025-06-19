a7fddb163905085469b702c136c3aa9da683d019
taco <gpg@tacowolf.net>
2025-06-18 22:51:03 -0400

    feat: and add it here

--- shell.nix
+++ shell.nix
@@ -7,6 +7,9 @@
   programs.zsh.profileExtra = ''
     [ -r ~/.nix-profile/etc/profile.d/nix.sh ] && source  ~/.nix-profile/etc/profile.d/nix.sh
     export XCURSOR_PATH=$XCURSOR_PATH:/usr/share/icons:~/.local/share/icons:~/.icons:~/.nix-profile/share/icons
+    eval "$(pay-respects bash --alias)"
+    eval "$(pay-respects zsh --alias)"
+    pay-respects fish --alias | source
   '';
   programs.zsh.enableCompletion = true;
   programs.zsh.enable = true;
