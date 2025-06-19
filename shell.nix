2f7926d18418ab1dd89e0316676a2159f7fcf9f3
taco <gpg@tacowolf.net>
2025-06-18 22:56:26 -0400

    fix: ok maybe like this instead

--- shell.nix
+++ shell.nix
@@ -7,9 +7,11 @@
   programs.zsh.profileExtra = ''
     [ -r ~/.nix-profile/etc/profile.d/nix.sh ] && source  ~/.nix-profile/etc/profile.d/nix.sh
     export XCURSOR_PATH=$XCURSOR_PATH:/usr/share/icons:~/.local/share/icons:~/.icons:~/.nix-profile/share/icons
-    eval "$(pay-respects bash --alias)"
-    eval "$(pay-respects zsh --alias)"
-    pay-respects fish --alias | source
+  '';
+  programs.zsh.initContent = ''
+  eval "$(pay-respects bash --alias)"
+  eval "$(pay-respects zsh --alias)"
+  pay-respects fish --alias | source
   '';
   programs.zsh.enableCompletion = true;
   programs.zsh.enable = true;
