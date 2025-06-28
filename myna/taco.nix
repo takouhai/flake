8340d930dc0722f7dbf4d0e633d987cd77a5f819
taco <gpg@tacowolf.net>
2025-06-28 12:46:20 -0400

    fix: add go bin path to machine

--- myna/taco.nix
+++ myna/taco.nix
@@ -1,6 +1,8 @@
 {...}: {
   home.username = "taco";
   home.homeDirectory = "/home/taco";
+
+  home.sessionPath = ["/usr/local/go/bin"];
   programs.zsh.initContent = ''
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
   '';
