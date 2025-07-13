05a6feee73febafc22411acd29b749a6f0422019
taco <gpg@tacowolf.net>
2025-07-12 20:04:14 -0400

    fix: actually i like this theme better lol

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,5 +1,5 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
-config.color_scheme = 'Dracula'
+config.color_scheme = 'Dracula+'
 config.font = wezterm.font 'ComicShannsMono Nerd Font Mono'
 return config
