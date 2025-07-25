d05d2cef3095789b2d49c0f1397eedb792ff7e77
taco <gpg@tacowolf.net>
2025-07-25 16:47:43 -0400

    fix: make the other command force things too

--- link.sh
+++ link.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-ln -s ~/.flake/.wezterm.lua ~/.wezterm.lua
+ln -fs ~/.flake/.wezterm.lua ~/.wezterm.lua
 ln -fs ~/.flake/.config/zed/settings.json ~/.config/zed/settings.json
 ln -fs ~/.flake/.config/topgrade.toml ~/.config/topgrade.toml
