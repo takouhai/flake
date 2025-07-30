7cdf8c7a3da40c77436d004004254708d47a4411
taco <gpg@tacowolf.net>
2025-07-30 18:19:37 -0400

    fix: volta compat

--- shell.nix
+++ shell.nix
@@ -7,6 +7,8 @@
   programs.zsh.profileExtra = ''
     [ -r ~/.nix-profile/etc/profile.d/nix.sh ] && source  ~/.nix-profile/etc/profile.d/nix.sh
     export XCURSOR_PATH=$XCURSOR_PATH:/usr/share/icons:~/.local/share/icons:~/.icons:~/.nix-profile/share/icons
+    export VOLTA_HOME="$HOME/.volta"
+    export PATH="$VOLTA_HOME/bin:$PATH"
   '';
   programs.zsh.enableCompletion = true;
   programs.zsh.enable = true;
